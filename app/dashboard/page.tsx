0fd5963d35658501becd6c4449972ef3300bd40b
SumitMahale259 <sumitmahale4006@gmail.com>
2026-04-13 16:06:05 +0530

    Updated code

--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-
-export default function Page() {
-    // return <Suspense fallback={<p>Loading...</p>}><p>Dashboard Page</p></Suspense>
-    return <p>Dashboard Page</p>
-}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -58,9 +58,9 @@ export async function fetchCardData() {
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+          FROM invoices`;
 
     const data = await Promise.all([
       invoiceCountPromise,
@@ -160,6 +160,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
